d4d201af164728ecffc82e354434e681b7e5f6bb
leeych <leeych@users.noreply.github.com>
2013-10-31 14:23:04 +0800

    翻译文件夹改名。

---
+++ resource/languages/simulator.ts
@@ -0,0 +1,132 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../src/macrostrings.h" line="6"/>
+        <source>Tip</source>
+        <translation>提示</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="7"/>
+        <source>Ok</source>
+        <translation>确定</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="8"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="9"/>
+        <source>Invalid</source>
+        <translation>非法</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="10"/>
+        <source>failed</source>
+        <translation>失败</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="12"/>
+        <source>Vehicle detector simulator</source>
+        <translation>车辆检测器触发测试工具</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="13"/>
+        <source>Already exists</source>
+        <translation>已经存在</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="14"/>
+        <source>Already assigned to</source>
+        <translation>已经分配给</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="15"/>
+        <source>Other</source>
+        <translation>其他</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="16"/>
+        <source>lane</source>
+        <translation>车道</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="17"/>
+        <source>Open</source>
+        <translation>打开</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="18"/>
+        <source>Close</source>
+        <translation>关闭</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="19"/>
+        <source>Serial port</source>
+        <translation>串口</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="20"/>
+        <source>Exists invalid lane id</source>
+        <translation>存在非法的车道号</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="22"/>
+        <source>Send msg</source>
+        <translation>发送数据</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="23"/>
+        <source>Start</source>
+        <translation>开始发车</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="24"/>
+        <source>Stop</source>
+        <translation>停止发车</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="25"/>
+        <source>Departure interval</source>
+        <translation>发车间隔</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="27"/>
+        <source>Port</source>
+        <translation>端口号</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="28"/>
+        <source>Baud rate</source>
+        <translation>波特率</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="29"/>
+        <source>Data bit</source>
+        <translation>数据位</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="30"/>
+        <source>Parity</source>
+        <translation>校验位</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="31"/>
+        <source>Stop bit</source>
+        <translation>停止位</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="33"/>
+        <source>Green arrow indicates come</source>
+        <translation>车辆进入</translation>
+    </message>
+    <message>
+        <location filename="../../src/macrostrings.h" line="34"/>
+        <source>Red arrow indicates left</source>
+        <translation>车辆离开</translation>
+    </message>
+</context>
+</TS>
